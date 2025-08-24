dd349cd9df81bff0cf98e348fccf1a5e4e21666f
amcaetano <antoniocaetano@protonmail.com>
2025-08-24 19:25:07 -0300

    Cofigurações basicas para uso do debiannn com tiling window manager i3wm

---
+++ pacotes_essenciais.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# SCRIPT PARA AUTOMATIZAR A INSTALAÇÃO DE PACOTES BASICOS NO DEBIAN COM O TILING WINDOW MANAGER I3WM
+
+	apt install xorg i3 nitrogen xfce4-power-manager lxappearance lightdm-settings pcmanfm neofetch xfce4-appfinder xfce4-terminal evince feh rofiq picom i3lock chromium vim fonts-open-sans fonts inconsolata pipewire grub-customizer 7zip file-roller unzip scrot cheese pavucontrol libspa-0.2-bluetooth mate-calc xrandr
+
+
+
+# OBS: Se o pacote do servidor de audio pipewire não der certo a opção seria o servidor padrão de distribuições linux o pulseaudio. Ferramenta file-roller é utilizada para compactar de descompactar arquivos de forma gráfica, xfce4-appfinder é uma aplicação utilizada para gerenciar arquivos instalados através da interface gráfica, evince é um leitor de arquivos no formato pdf.
+
+# OBS: O pacote do scrot é necessário para servir um utilitário simples para captura de tela e o cheese para webcam.
+   
+# Comando para configurar a resolução de tela no ambiente i3wm  
+#xrandr -s 1920x1080
+ 
+
+
